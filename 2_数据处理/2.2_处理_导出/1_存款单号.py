68923dc36ce1a3779e15403d199c12fd956c58d9
Poh Hok Jun <pohhokjun@gmail.com>
2025-04-26 20:50:01 +0700

    鑷姩鎻愪氦

--- 2_数据处理/2.2_处理_导出/1_存款单号.py
+++ 2_数据处理/2.2_处理_导出/1_存款单号.py
@@ -5,8 +5,8 @@ import os
 from sqlalchemy import create_engine
 
 # 时间范围（便于修改）
-START_TIME = '2025-04-24 00:00:00'  # 开始时间
-END_TIME = '2025-04-24 23:59:59'    # 结束时间
+START_TIME = '2025-04-25 00:00:00'  # 开始时间
+END_TIME = '2025-04-25 23:59:59'    # 结束时间
 
 # 全局映射字典
 PAY_METHODS = {
